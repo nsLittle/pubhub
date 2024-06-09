d9ee2d2090f029b196a2702a42388f0bba77647e
Stephen <stephen.swaringin@gmail.com>
2024-06-09 07:00:17 -0500

    Improving brewery selection functionality

--- app/components/BreweryCard.jsx
+++ app/components/BreweryCard.jsx
@@ -1,29 +1,28 @@
 'use client'
-import { useState } from "react";
 
 /**
  * A component to display an individual brewery
  * @returns Functional React Component
  */
-export function BreweryCard({ brewery }) {
-  const [cardState, setCardState] = useState('');
+export function BreweryCard({ brewery, isActive, handleCardClick }) {
 
-  function handleCardClick(){
-    if (cardState === 'active') {
-      setCardState('');
-    } else {
-      setCardState('active')
-    }
-  }
-  
   return (
-    <div 
-      className={`list-group-item list-group-item-action ${cardState} mb-2 rounded-1 breweryCard d-flex`}
-      onClick={handleCardClick}
-    >
-    {/* <div className="list-group-item"> */}
-      <h3 className="m-1">{brewery.name}</h3>
-      {/* <small>{brewery.website_url}</small> */}
-    </div>
+    (isActive ?
+      <div 
+        className={`list-group-item list-group-item-action active mb-2 rounded-1 breweryCard d-flex`}
+        onClick={handleCardClick}
+      >
+        {/* <div className="list-group-item"> */}
+        <h3 className="m-1">{brewery.name}</h3>
+        {/* <small>{brewery.website_url}</small> */}
+      </div>
+      : 
+      <div 
+        className={`list-group-item list-group-item-action mb-2 rounded-1 breweryCard d-flex`}
+        onClick={handleCardClick}
+      >
+        <h3 className="m-1">{brewery.name}</h3>
+      </div>
+    )
   );
 }
--- app/components/BreweryList.jsx
+++ app/components/BreweryList.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { BreweryCard } from "./BreweryCard";
 import { useSelector } from 'react-redux';
+import { act, useState } from "react";
 
 /**
  * A component to display the list of brewery cards
@@ -9,12 +10,26 @@ import { useSelector } from 'react-redux';
 export function BreweryList() {
   // Pulls the array of breweries stored in Redux
   const breweries = useSelector((state) => state.breweries.breweries);
+  // Keeps track of if a brewery card has been selected (with nothing being selected initially)
+  const [activeCard, setActiveCard] = useState(0);
 
   return (
     <div className="col-md-4 p-4">
       <div className="list-group">
         {/* Maps breweries to individual cards on the page */}
-        {breweries.map(brewery => (<BreweryCard key={brewery.id} brewery={brewery}/>))}
+        {breweries.map(brewery => (
+          <BreweryCard 
+            key={brewery.id}
+            brewery={brewery}
+            isActive={activeCard === brewery.id}
+            handleCardClick={() => {
+              // Sets an active BreweryCard.
+              // Multiple BreweryCards cannot be selected at the same time.
+              // If the active BreweryCard is already selected, it deselects the card.
+              activeCard === brewery.id ? setActiveCard(0) : setActiveCard(brewery.id)
+            }}
+          />
+        ))}
       </div>
     </div>
   );
